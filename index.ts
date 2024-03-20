bde6352dfc76ea10bf6b1d9f44f5ebda03da2fee
luzzifoss <fedeluzzi00@gmail.com>
2024-03-20 16:09:42 +0000

    chore: reintroduce supported chain enum

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import type { ChainContract } from "viem";
 import { celoAlfajores } from "viem/chains";
 
-export const ADDRESS: Record<number, ChainContract> = {
-    [celoAlfajores.id]: {
+export enum SupportedChain {
+    CeloAlfajores = celoAlfajores.id,
+}
+
+export const ADDRESS: Record<SupportedChain, ChainContract> = {
+    [SupportedChain.CeloAlfajores]: {
         address: "0xe82c4D8b993D613a28600B953e91A3A93Ae69Fd6",
         blockCreated: 23_111_863,
     },
