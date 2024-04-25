ad969f7a06c672f6367ed7d6c1b6fd139f446835
luzzifoss <fedeluzzi00@gmail.com>
2024-04-25 11:49:07 +0200

    chore: update variable name in deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,13 +9,13 @@ contract Deploy is Script {
     function run(
         address _owner,
         address _updater,
-        uint32 _fee,
+        uint32 _globalFee,
         uint32 _minimumCampaignDuration,
         uint32 _maximumCampaignDuration
     ) public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        Metrom _metrom = new Metrom(_owner, _updater, _fee, _minimumCampaignDuration, _maximumCampaignDuration);
+        Metrom _metrom = new Metrom(_owner, _updater, _globalFee, _minimumCampaignDuration, _maximumCampaignDuration);
         console2.log("Metrom address: ", address(_metrom));
 
         vm.stopBroadcast();
