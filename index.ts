d10f06afd56ed564b40fe00ea00e0e1c70c55fc4
luzzifoss <fedeluzzi00@gmail.com>
2024-06-03 13:49:50 +0100

    chore: v0.8.0

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ export enum SupportedChain {
 
 export const ADDRESS: Record<SupportedChain, ChainContract> = {
     [SupportedChain.Holesky]: {
-        address: "0x8d98758EfF88Dc944035B0618a8412C400E71C72",
-        blockCreated: 1561518,
+        address: "0x10E1A22034C5AF1E793c2Ac189b90ca47b252fF9",
+        blockCreated: 1663871,
     },
 };
