4874e092a78157d5ed9811b87f8cf1c481355352
luzzif <federico.luzzi@proton.me>
2024-06-28 09:58:22 +0100

    chore: new holesky deployments and add back celo alfajores support

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import type { ChainContract } from "viem";
-import { holesky } from "viem/chains";
+import { holesky, celoAlfajores } from "viem/chains";
 
 export enum Environment {
     Development = "development",
@@ -8,6 +8,7 @@ export enum Environment {
 
 export enum SupportedChain {
     Holesky = holesky.id,
+    CeloAlfajores = celoAlfajores.id,
 }
 
 export const ADDRESS: Record<
@@ -16,14 +17,22 @@ export const ADDRESS: Record<
 > = {
     [Environment.Development]: {
         [SupportedChain.Holesky]: {
-            address: "0x2d2E7dC3c5CAD9020198b5FDDEc548cdBf079F68",
-            blockCreated: 1823707,
+            address: "0x05B92b5C40a266EFDD8B3fDF0496407e8C0d9cB6",
+            blockCreated: 1823746,
+        },
+        [SupportedChain.CeloAlfajores]: {
+            address: "0x10E1A22034C5AF1E793c2Ac189b90ca47b252fF9",
+            blockCreated: 24833828,
         },
     },
     [Environment.Staging]: {
         [SupportedChain.Holesky]: {
-            address: "0x2B428A243Af4ce5fF4E25a4E392708A4A020d28C",
-            blockCreated: 1823713,
+            address: "0x4A827f3Bf3c38Baa091DdCAb7B801aCee6819759",
+            blockCreated: 1823756,
+        },
+        [SupportedChain.CeloAlfajores]: {
+            address: "0xd1c1153fd809Aae3bb431b586C032C4856abaeD4",
+            blockCreated: 24833840,
         },
     },
 };
