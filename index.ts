291cb4270108aee397204ff56cd926acfa59a60d
luzzif <federico.luzzi@proton.me>
2024-07-31 14:39:47 +0100

    chore: new deployments

--- index.ts
+++ index.ts
@@ -17,30 +17,30 @@ export const ADDRESS: Record<
 > = {
     [Environment.Development]: {
         [SupportedChain.Holesky]: {
-            address: "0x05B92b5C40a266EFDD8B3fDF0496407e8C0d9cB6",
-            blockCreated: 1823746,
+            address: "0xBB7a0d06c6f37B1607bceeaFA452216BC843C193",
+            blockCreated: 2039209,
         },
         [SupportedChain.CeloAlfajores]: {
-            address: "0x10E1A22034C5AF1E793c2Ac189b90ca47b252fF9",
-            blockCreated: 24833828,
+            address: "0x766faa004398d68Ef7f64926525E7ad2933A0f87",
+            blockCreated: 25404664,
         },
         [SupportedChain.MantleSepolia]: {
-            address: "0xe3dA4E4b76C4ed3e4227db20F20d1F25A4507f9b",
-            blockCreated: 9840417,
+            address: "0x080a71eC6Cb5C67480DE006A59d7991cD8fD2329",
+            blockCreated: 10444146,
         },
     },
     [Environment.Staging]: {
         [SupportedChain.Holesky]: {
-            address: "0x4A827f3Bf3c38Baa091DdCAb7B801aCee6819759",
-            blockCreated: 1823756,
+            address: "0x44d112ACD0764960667a7b9eA95B8120f39f58C4",
+            blockCreated: 2039220,
         },
         [SupportedChain.CeloAlfajores]: {
-            address: "0xd1c1153fd809Aae3bb431b586C032C4856abaeD4",
-            blockCreated: 24833840,
+            address: "0x52015b596e765F7AA2668BCC30Fc715748A20A8e",
+            blockCreated: 25404677,
         },
         [SupportedChain.MantleSepolia]: {
-            address: "0xBbB06b25484AB9E23FEe8Ee321Af8e253ea7A76a",
-            blockCreated: 9840495,
+            address: "0x87d24272071593B4a7907fd133E74EC30025D4F9",
+            blockCreated: 10444197,
         },
     },
 };
